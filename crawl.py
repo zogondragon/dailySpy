906d1f6e2d2228ed6cca84069375a2bae4064b5d
Gunhee Lee <teuksu00@gmail.com>
2014-09-19 14:23:46 +0900

    * It can retrieve "testing the test" search result from Google.

--- crawl.py
+++ crawl.py
@@ -1,5 +1,7 @@
 # crawl.py : This script send the query to Google.
+from google import search
 
 if __name__ == '__main__':
 	print("Hello World!")
-
+	for url in search('testing the test', stop=10):
+		print(url)
