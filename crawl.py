c8a874ca392df367b8b99c874e4ea4d234460f2d
Gunhee Lee <teuksu00@gmail.com>
2014-09-19 22:28:18 +0900

    * Now print to stdout and logfile in logs/.

--- crawl.py
+++ crawl.py
@@ -1,14 +1,19 @@
-# crawl.py : This script send the query to Google.
+# crawl.py : This script send the query to Google and save the result as a log file.
 from google import search
 import datetime
 
 if __name__ == '__main__':
-	# print datetime
+	filename = str(datetime.datetime.now()) + '.log'
 	print(datetime.datetime.now())
+
+	logfile = open('logs/' + filename, 'a')
 
 	count = 0
 	for url in search('북조선', stop=100):
 		print(url)
+		logfile.write(url + '\n')
 		count += 1
 
 	print(count)
+	logfile.write(str(count) + '\n')
+
