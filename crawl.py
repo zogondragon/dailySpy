a805409f423c552930bc7a0b9ef38521583ea4ec
Gunhee Lee <teuksu00@gmail.com>
2014-09-22 19:25:05 +0900

    * Google ban circumvented (by clearing cookie regularly).

--- crawl.py
+++ crawl.py
@@ -4,20 +4,21 @@
 from google import search
 import datetime
 import time
+import os
 
 
 def GoogleAKeyword(queryString, outputFile):
 	"Send a string to Google.com and log the result as a file. Default stop=100."
 	outputFile.write("==Search keyword: " + queryString + "==\n")
-	print("==Search keyword: " + queryString + "==")
+	#print("==Search keyword: " + queryString + "==")
 
 	count = 0
-	for url in search(queryString, stop=100, pause=5.0):
-		print(url)
+	for url in search(queryString, stop=50, pause=10.0):
+		#print(url)
 		outputFile.write(url + '\n')
 		count += 1
 
-	print(count)
+	#print(count)
 	outputFile.write(str(count) + '\n')
 
 
@@ -33,4 +34,5 @@ if __name__ == '__main__':
 	for line in keywordFile:
 		GoogleAKeyword(line.rstrip('\n'), logfile)
 		time.sleep(120) #  to circumvent Google's blocking.
+		os.remove("/root/.google-cookie")
 
